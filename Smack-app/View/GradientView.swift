4e0534755882be81da014e56e4ef2a1b8e0c5f4e
Hasan Akoglu <hasanakoglu@Hasans-MBP.warley.ford.com>
2017-12-14 11:59:10 +0000

    end of part 3

--- Smack-app/View/GradientView.swift
+++ Smack-app/View/GradientView.swift
@@ -15,22 +15,26 @@ import SnapKit
 class GradientView: UIView {
 
   @IBInspectable
-    var topColour: UIColor = UIColor.blue {
+    var topColour: UIColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) {
     didSet {
-        self.setNeedsLayout() //when we set it, this updates the layout of the view
+        self.setNeedsLayout() //when we set it, this updates the layout of the view dynamically
         }
     }
     
     @IBInspectable
-    var bottomColour: UIColor = UIColor.green {
+    var bottomColour: UIColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) {
         didSet {
             self.setNeedsLayout() //when we set it, this updates the layout of the view
         }
     }
-    
+    //this is called whenever we change the layer 
     override func layoutSubviews() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [topColour.cgColor, bottomColour.cgColor]
+        gradientLayer.startPoint = CGPoint(x:0 , y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds //to equal it to the size of the uiview
+        self.layer.insertSublayer(gradientLayer, at: 0) //add gradient layer to the uiview layer
         
     }
     
